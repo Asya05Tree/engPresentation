e16cc3a10e097f3e1ae1b260300a9689b36dfbee
Nasya_K <71969033+Asya05Tree@users.noreply.github.com>
2025-02-20 20:20:42 +0200

    Update language.js

--- js/language.js
+++ js/language.js
@@ -1,7 +1,1 @@
-export function toggleLanguage() {
-    document.body.classList.toggle('uk');
-}
 
-export function getCurrentLanguage() {
-    return document.body.classList.contains('uk') ? 'uk' : 'en';
-}
