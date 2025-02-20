df42285e4795989c9e5af5b5da75bb2d472a4bfd
Nasya_K <71969033+Asya05Tree@users.noreply.github.com>
2025-02-20 14:28:18 +0200

    Delete engPresentation directory

--- engPresentation/js/language.js
+++
@@ -1,7 +0,0 @@
-export function toggleLanguage() {
-    document.body.classList.toggle('uk');
-}
-
-export function getCurrentLanguage() {
-    return document.body.classList.contains('uk') ? 'uk' : 'en';
-}
